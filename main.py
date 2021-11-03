0be133c4f1ab9e4b0f9207ce739536d0b1b10f1a
wafforyu <wafforyu@gmail.com>
2021-11-04 00:48:15 +0800

    test tkinter window

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import numpy as np
 import sympy as sp
-import tkinter as t
+from tkinter import *
 from IPython.display import display, Math
 from input import *
 from matrix import *
 
 
 def main():
-    a = Matrix(inp_length(), inp_height())
-    a.enter_values()
-    a.print_matrix()
+    window = Tk()
+
+    # create objects
+    label = Label(window, text="Matrix Operations")
+    text = Text(window, cnf={'bg': 'blue'})
+    button = Button(window, text="button")
+
+    # show objects in screen
+    label.pack()
+    text.pack()
+    button.pack()
+
+    # run window
+    window.mainloop()
+
+# a = Matrix(inp_length(), inp_height())
+# a.enter_values()
+# a.print_matrix()
 
 
 if __name__ == '__main__':
