0b1fbc9887afebb7fc52b506314dfc3e32bd05d1
wafforyu <wafforyu@gmail.com>
2021-11-04 03:14:12 +0800

    add 2nd window

--- menu.py
+++ menu.py
@@ -1,29 +1,31 @@
 from tkinter import *
+from operations import *
 
+
+gui_menu = Tk(className="Mtx Operations")
 
 
 class Menu:
     def __init__(self):
-        menu = Tk(className="Mtx Operations")
-        menu.geometry("150x180")
-        # menu['background'] = '#121212' # crappy
+        gui_menu.geometry("150x180")
+        # gui_menu['background'] = '#121212' # crappy
 
         # create label
-        text = Label(menu, text="Choose Operation:", pady=10)
+        label = Label(gui_menu, text="Choose Operation:", pady=10)
 
-        inv = Button(menu, text="Inverse", padx=30, pady=5)
-        add = Button(menu, text="Add", padx=40, pady=5)
-        sub = Button(menu, text="Subtract", padx=25, pady=5)
-        mlt = Button(menu, text="Multiply", padx=28, pady=5)
+        inv = Button(gui_menu, text="Inverse", padx=30, pady=5, command=inverse_op)
+        add = Button(gui_menu, text="Add", padx=40, pady=5)
+        sub = Button(gui_menu, text="Subtract", padx=25, pady=5)
+        mlt = Button(gui_menu, text="Multiply", padx=28, pady=5)
 
-        text.pack()
+        label.pack()
         inv.pack()
         add.pack()
         sub.pack()
         mlt.pack()
 
         # run window
-        menu.mainloop()
+        gui_menu.mainloop()
 
         # a = Matrix(inp_length(), inp_height())
         # a.enter_values()
--- operations.py
+++ operations.py
@@ -1,2 +1,20 @@
+from tkinter import *
+from menu import *
+import menu
+
+
+def inverse_op():
+    menu.gui_menu.destroy()
+    frame1 = Tk()
+    # enter matrix length
+    # enter matrix height
+    # enter elements based on length and height inputs
+    # calculate
+    # output window
+    # go back
+    frame1.mainloop()
+
+
 class Operations:
-    
+    def __init__(self):
+        pass
