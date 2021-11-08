c372a6dcdbbb174808889fb60a76494f285da87e
wafforyu <wafforyu@gmail.com>
2021-11-08 14:56:40 +0800

    add exceptions

--- menu.py
+++ menu.py
@@ -1,15 +1,16 @@
 from tkinter import Tk, Label, Button
-import inverse 
-
+import inverse
 
 gui_menu = Tk()
 gui_menu.geometry('150x180')
 gui_menu.title('Menu')
+
+
 class Menu:
     def __init__(self):
         label = Label(gui_menu, text="Choose Operation:", pady=10, font=('arial', 10, 'bold'))
 
-        inv = Button(text="Inverse", padx=30, pady=5, command=inverse.Inverse) 
+        inv = Button(text="Inverse", padx=30, pady=5, command=inverse.Inverse)
         add = Button(gui_menu, text="Add", padx=40, pady=5)
         sub = Button(gui_menu, text="Subtract", padx=25, pady=5)
         mlt = Button(gui_menu, text="Multiply", padx=28, pady=5)
