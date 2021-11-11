446a86ec72fa2592ceeb5ab7f668a107171b19ef
wafforyu <wafforyu@gmail.com>
2021-11-12 03:52:02 +0800

    add tranpose

--- trans.py
+++ trans.py
@@ -3,9 +3,127 @@ from tkinter.constants import BOTH
 import numpy as np
 import menu
 
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
 
 class Trans:
+    def back_to_menu(self):
+        self.gui_trans_output.destroy()
+        menu.gui_menu.deiconify()
+
+    def compute_transpose(self):
+        try:
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    self.matrix[i][j] = int(self.matrix[i][j])
+
+            list_mat = [str(i) for i in np.transpose(self.matrix)]
+
+            # remove square brackets
+            for i in range(len(list_mat)):
+                list_mat[i] = list_mat[i][1:-1]
+            return list_mat
+
+        except TypeError or Exception:
+            pass
+
+    def output_matrix(self):
+        self.gui_trans_input.destroy()
+        self.gui_trans_output = Toplevel()
+        self.gui_trans_output.title("Transpose")
+        self.gui_trans_output.resizable(False, False)
+
+        self.frame_trans_output= Frame(self.gui_trans_output, highlightbackground='black', highlightthickness=1)
+        self.frame_trans_output.pack(fill=BOTH, expand=True, padx=5, pady=5)
+
+        # go back to menu button
+        Button(self.frame_trans_output, text="Back", width=4, command=self.back_to_menu).grid(
+            row=self.rows + self.rows + 10, column=1)
+
+        Label(self.frame_trans_output, text='Input:', font=('arial', 10, 'bold'), underline=0).grid(row=1, column=1)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                Label(self.frame_trans_output, text=self.matrix[i][j], bd=5).grid(row=i + 1, column=j + 2)
+
+        # display output
+        Label(self.frame_trans_output, text='Output:', font=('arial', 10, 'bold'), underline=0).grid(
+            row=self.rows * 2,
+            column=1)
+
+        self.transposed_matrix = self.compute_transpose()
+        for i in range(self.cols):
+            Label(self.frame_trans_output, text=self.transposed_matrix[i], bd=5).grid(
+                row=i + self.rows * 2, column=2, columnspan=self.cols, sticky='w ')
+
+        self.gui_trans_output.protocol("WM_DELETE_WINDOW", menu.gui_menu.destroy)
+        self.gui_trans_output.mainloop()
+
+    def input_matrix(self):
+        self.gui_trans_menu.destroy()
+        self.gui_trans_input = Toplevel()
+        self.gui_trans_input.title("Transpose")
+        self.gui_trans_input.resizable(False, False)
+
+        self.frame_trans_input = Frame(self.gui_trans_input, highlightbackground='black', highlightthickness=1)
+        self.frame_trans_input.pack(fill=BOTH, expand=True, padx=5, pady=5)
+        # window_dimensions = str(self.m_length.get()**3+90) + "x" + str(self.m_height.get())
+        # print(window_dimensions)
+        # window.geometry(window_dimensions)
+        # self.gui_inverse_input.resizable(False, False)
+
+        Label(self.frame_trans_input, text="Enter matrix:", font=('arial', 10, 'bold')).grid(row=1, column=1)
+        # to create matrix of entry cells we need to create a 2d list of entries
+        # thank god to stackoverflow peeps for that
+
+        # empty arrays for Entry and StringVars
+        text_var = []
+        entries = []
+
+        self.rows, self.cols = (self.rows.get(), self.cols.get())
+        for i in range(self.rows):
+            # append an empty list to arrays to append to later
+            text_var.append([])
+            entries.append([])
+            for j in range(self.cols):
+                # for column indications
+                if i == 1:
+                    Label(self.frame_trans_input, text=alphabet[j]).grid(row=1, column=j + 2)
+
+                # append StringVar
+                text_var[i].append(StringVar())
+
+                # append the entry into the list
+                entries[i].append(Entry(self.frame_trans_input, textvariable=text_var[i][j], width=3))
+
+                # display entry
+                entries[i][j].grid(row=i + 2, column=j + 2)
+
+                # for row indications
+                Label(self.frame_trans_input, text=i + 1).grid(row=i + 2, column=1, sticky='e')
+
+        def get_mat():
+            try:
+                self.matrix = []
+                for i2 in range(self.rows):
+                    self.matrix.append([])
+                    for j2 in range(self.cols):
+                        self.matrix[i2].append(text_var[i2][j2].get())
+                self.output_matrix()
+            except ValueError or Exception:
+                pass
+
+        Button(self.frame_trans_input, text="Enter", width=8, command=get_mat).grid(row=self.cols + self.cols + 10, column=1)
+
+        self.gui_trans_input.protocol("WM_DELETE_WINDOW", menu.gui_menu.destroy)
+        self.gui_trans_input.mainloop()
+
     def __init__(self):
+        self.gui_trans_input = None
+        self.frame_trans_input = None
+        self.gui_trans_output = None
+        self.frame_trans_output = None
+        self.transposed_matrix = None
+
         menu.gui_menu.withdraw()
         self.gui_trans_menu = Toplevel()
         self.gui_trans_menu.title("Transpose")
@@ -27,7 +145,7 @@ class Trans:
         self.cols.set(2)
         OptionMenu(self.frame_trans_menu, self.cols, *range(2, 5)).grid(row=1, column=4)
 
-        Button(self.frame_trans_menu, text='Enter', padx=16, pady=5).grid(row=2, column=4)
+        Button(self.frame_trans_menu, text='Enter', padx=16, pady=5, command=self.input_matrix).grid(row=2, column=4)
 
         self.gui_trans_menu.protocol("WM_DELETE_WINDOW", menu.gui_menu.destroy)
         self.gui_trans_menu.mainloop()
